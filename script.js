c1d18a586ae65571ddd9e1c860cee7b79737bf1b
Atibakh <83715359+Atibakh@users.noreply.github.com>
2023-03-06 11:28:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// what would you do in a day
+
+// random number function
+function randomNumber() {
+  return Math.floor(Math.random() * 10);
+}
+
+console.log(randomNumber());
+console.log("Hello");
